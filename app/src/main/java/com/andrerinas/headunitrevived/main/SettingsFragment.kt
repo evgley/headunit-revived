ad7ea2aa61392a6f1b056c11cc78b8ab9702df5f
andrecuellar <acuellaravaroma@gmail.com>
2026-03-29 02:03:48 -0400

    Move vehicle info back to General, keep it out of Automation
    Vehicle information has nothing to do with automation. Moved it back
    to General section, before the wifi connection mode entry.

--- app/src/main/java/com/andrerinas/headunitrevived/main/SettingsFragment.kt
+++ app/src/main/java/com/andrerinas/headunitrevived/main/SettingsFragment.kt
@@ -376,6 +376,17 @@ class SettingsFragment : Fragment() {
             }
         ))
 
+        items.add(SettingItem.SettingEntry(
+            stableId = "vehicleInfoSettings",
+            nameResId = R.string.vehicle_info_settings,
+            value = getString(R.string.vehicle_info_settings_description),
+            onClick = {
+                try {
+                    findNavController().navigate(R.id.action_settingsFragment_to_vehicleInfoFragment)
+                } catch (e: Exception) { }
+            }
+        ))
+
         val wifiModes = resources.getStringArray(R.array.wireless_connection_modes)
         items.add(SettingItem.SettingEntry(
             stableId = "wifiConnectionMode",
@@ -463,17 +474,6 @@ class SettingsFragment : Fragment() {
                     checkChanges()
                     updateSettingsList()
                 }
-            }
-        ))
-
-        items.add(SettingItem.SettingEntry(
-            stableId = "vehicleInfoSettings",
-            nameResId = R.string.vehicle_info_settings,
-            value = getString(R.string.vehicle_info_settings_description),
-            onClick = {
-                try {
-                    findNavController().navigate(R.id.action_settingsFragment_to_vehicleInfoFragment)
-                } catch (e: Exception) { }
             }
         ))
 
