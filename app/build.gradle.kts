b11f66ce3364a570ca13d5cb010c424977f68261
ars <andre.rinas@netempire.software>
2026-05-14 14:14:10 +0200

    final steps for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,8 +100,8 @@ android {
         applicationId = "com.andrerinas.headunitrevived"
         minSdk = 16
         targetSdk = 36
-        versionCode = 66
-        versionName = "2.3.0-beta2"
+        versionCode = 67
+        versionName = "2.3.0"
         setProperty("archivesBaseName", "${applicationId}_${versionName}")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
